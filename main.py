59d1d6b85c71ff4d0ef02a181aa8e641963782b3
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-06-14 00:20:51 +0200

    add name main idiom

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from display_responses import DisplayResponses
 from parse_xml import ParseXml
 
-xml_file = 'scanner.xml'
-parsed_xml = ParseXml(xml_file)
-hosts = parsed_xml.get_hosts()
-commands = parsed_xml.get_commands()
+if __name__ == "__main__":
+    xml_file = 'scanner.xml'
+    parsed_xml = ParseXml(xml_file)
+    hosts = parsed_xml.get_hosts()
+    commands = parsed_xml.get_commands()
 
-responses = DisplayResponses(hosts, commands)
-responses.display_table()
+    responses = DisplayResponses(hosts, commands)
+    responses.display_table()
 
